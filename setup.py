a29ad18490b551b96c22a84a82e97deaf77c2cb7
Nicolas Le Manchet <nicolas@lemanchet.fr>
2018-11-04 19:59:21 +0100

    Run tests on Python 3.7

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='bplustree B+tree Btree database index',
 
